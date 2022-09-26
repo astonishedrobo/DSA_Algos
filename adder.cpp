b807463fd5b8064e7d5e0fa508dc3db996043907
Soumyajit <soumyajit.basu22@gmail.com>
2022-09-26 15:01:57 +0530

    initial commit

---
+++ adder.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+    int x, y;
+    cout << "Enter two numbers: " << endl;
+    cin >> x >> y;
+    cout << "sum: " << x+y << endl;
+}
