257002d9a2f89e160b22fd1bfbddb078ca8a2dac
Soumyajit <soumyajit.basu22@gmail.com>
2022-09-26 16:05:31 +0530

    adding multiplication

---
+++ mult.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+    int x,y;
+    cout << "Enter two numbers for multiplication" << endl;
+    cin >> x, y;
+    cout << "Result: " << x*y << endl;
+}
